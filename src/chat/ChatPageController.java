cf748c996a58c459b80e4771ba4ed4074d9ed05c
Marcel Lipczynski <marcellipczynski@gmail.com>
2019-10-28 20:54:41 +0100

    Added server address field and port field

--- src/chat/ChatPageController.java
+++ src/chat/ChatPageController.java
@@ -17,9 +17,10 @@ import java.util.List;
 public class ChatPageController {
 
     private List<String> messages = new ArrayList<>();
+    private List<String> users = new ArrayList<>();
 
     @FXML
-    private Button buttonSend;
+    private ListView userList;
 
     @FXML
     private TextArea messageBox;
@@ -30,9 +31,13 @@ public class ChatPageController {
     @FXML
     public void handleSendButtonClick() {
         String text = messageBox.getText();
+
         messages.add(text);
+
         chatPane.getItems().setAll(messages);
         chatPane.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+
 
         messageBox.clear();
     }
