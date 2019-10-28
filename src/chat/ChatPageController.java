d71e237cb6884a4c3cfc6a3f541af6406552a71c
Marcel Lipczynski <marcellipczynski@gmail.com>
2019-10-28 20:37:13 +0100

    Added sending messages by clicking send button and by hitting enter key

---
+++ src/chat/ChatPageController.java
@@ -0,0 +1,46 @@
+package chat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChatPageController {
+
+    private List<String> messages = new ArrayList<>();
+
+    @FXML
+    private Button buttonSend;
+
+    @FXML
+    private TextArea messageBox;
+
+    @FXML
+    private ListView chatPane;
+
+    @FXML
+    public void handleSendButtonClick() {
+        String text = messageBox.getText();
+        messages.add(text);
+        chatPane.getItems().setAll(messages);
+        chatPane.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        messageBox.clear();
+    }
+
+    @FXML
+    public void handleEnterKey(KeyEvent event) throws IOException {
+        if (event.getCode() == KeyCode.ENTER) {
+            handleSendButtonClick();
+        }
+    }
+}
